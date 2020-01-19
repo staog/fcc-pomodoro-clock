0d17a839e7e7782d085e8b8533f397d01f23df6c
Milan V. Kecojević <staog@gmx.com>
2020-01-19 20:38:13 +0100

    Fix 60 minute session problem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+const sound = document.getElementById('beep')
 
 class Pomodoro extends React.Component {
   constructor(props) {
@@ -8,9 +6,9 @@ class Pomodoro extends React.Component {
     this.state = {
       breakMinutes: 5,
       sessionMinutes: 25,
+      active: false,
       type: "session",
-      breakTime: 5 * 60 * 1000,
-      sessionTime: 25 * 60 * 1000,
+      currentTime: 25 * 60 * 1000
     }
   }
 
@@ -18,31 +16,28 @@ class Pomodoro extends React.Component {
     let breakMinutes = this.state.breakMinutes
     let sessionMinutes = this.state.sessionMinutes
     let type = this.state.type
-    let breakTime = this.state.breakTime
-    let sessionTime = this.state.sessionTime
+    let currentTime = this.state.currentTime
     switch (true) {
       case /\bbreak-increment\b/.test(e.target.id):
         if (breakMinutes < 60) {
           breakMinutes += 1
-          breakTime += 60000
         }
         break
       case /\bbreak-decrement\b/.test(e.target.id):
-        if (breakMinutes > 0) {
+        if (breakMinutes > 1) {
           breakMinutes -= 1
-          breakTime -= 60000
         }
         break
       case /\bsession-increment\b/.test(e.target.id):
         if (sessionMinutes < 60) {
           sessionMinutes += 1
-          sessionTime += 60000
+          currentTime += 60000
         }
         break
       case /\bsession-decrement\b/.test(e.target.id):
-        if (sessionMinutes > 0) {
+        if (sessionMinutes > 1) {
           sessionMinutes -= 1
-          sessionTime -= 60000
+          currentTime -= 60000
         }
         break
     }
@@ -50,46 +45,50 @@ class Pomodoro extends React.Component {
       breakMinutes: breakMinutes,
       sessionMinutes: sessionMinutes,
       type: type,
-      breakTime: breakTime,
-      sessionTime: sessionTime
+      currentTime: currentTime
     })
   }
 
-  // countDown() {
-  //   let sessionTime = this.state.sessionTime
-  //   sessionTime -= 1000
-  //   this.setState({
-  //     sessionTime: sessionTime
-  //   })
-  // }
-  //
-  // function startTimer = setInterval(countDown, 1000)
-  //
-  // handlePlayAndPause = e => {
-  //   if (e.target.id === "start_stop") {
-  //     startTimer()
-  //   }
-  // }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.currentTime === 0 && prevState.type === 'session') {
+      this.setState({ currentTime: this.state.breakMinutes * 60 * 1000, type: 'break'})
+      sound.pause()
+      sound.currentTime = 0
+      sound.play()
+    }
+    if (prevState.currentTime === 0 && prevState.type === 'break') {
+      this.setState({ currentTime: this.state.sessionMinutes * 60 * 1000, type: 'session'})
+      sound.pause()
+      sound.currentTime = 0
+      sound.play()
+    }
+  }
 
-  // Treba da uglavim negde u funkciju da mi pusta zvuk
-  // let sound = document.getElementById('beep')
-  // this.playAudio(sound)
+  handlePlayAndPause = e => {
+    if (e.target.id === "start_stop") {
+      if (this.state.active) {
+        clearInterval(this.countDown)
+        this.setState({ active: false })
+      }
+      else {
+        this.countDown = setInterval(() => this.setState({currentTime: this.state.currentTime - 1000, active: true}), 1000)
+      }
+    }
+  }
 
   handleReset = e => {
     if (e.target.id === "reset") {
       this.setState({
         breakMinutes: 5,
         sessionMinutes: 25,
+        active: false,
         type: "session",
-        breakTime: 5 * 60 * 1000,
-        sessionTime: 25 * 50 * 1000
+        currentTime: 25 * 60 * 1000
       })
+      clearInterval(this.countDown)
+      sound.pause()
+      sound.currentTime = 0
     }
-  }
-
-  playAudio = (sound) => {
-    sound.currentTime = 0
-    sound.play()
   }
 
   render() {
@@ -101,7 +100,7 @@ class Pomodoro extends React.Component {
         <br />
         <div id="timer">
           <div id="timer-label">{this.state.type === "session" ? "Session" : "Break"}</div>
-          <div id="time-left">{this.state.sessionTime}</div>
+          <div id="time-left">{this.state.currentTime === 3600000 ? "60:00" : moment(this.state.currentTime).format("mm:ss")}</div>
         </div>
         <div className="row">
           <div className="two-halfs">
@@ -123,7 +122,7 @@ class Pomodoro extends React.Component {
         </div>
         <br />
         <div className="row">
-          <button id="start_stop" className="controls" active={this.state.type} onClick={this.handlePlayAndPause}><i className="fa fa-play"></i>/<i className="fa fa-pause"></i></button>
+          <button id="start_stop" className="controls" type={this.state.type} active={this.state.active} onClick={this.handlePlayAndPause}>{ this.state.active ? <i className="fa fa-pause"></i> : <i className="fa fa-play"></i> }</button>
           <button id="reset" className="controls" onClick={this.handleReset}><i className="fa fa-refresh"></i></button>
         </div>
       </div>
